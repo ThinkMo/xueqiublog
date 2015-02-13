4c237e08d6391a0f358c80c6e1ed7d03cf99e286
juncaofish <jun.cao@ni.com>
2015-02-13 17:41:47 +0800

    Modify the script to grab several IDs
    With some blogs

--- blogGrabber.py
+++ blogGrabber.py
@@ -7,10 +7,10 @@ import json
 import requests
 import urllib
 
-_url = "http://xueqiu.com/v4/statuses/user_timeline.json?user_id=2821861040&page={0}&type=1"
-reg = re.compile(r'<h4 class="status-title">(.*)</h4>(.*)</script>(.*)<!-- pdf--></div>')
-imgreg = re.compile('<img class="ke_img" src="(.*?)" />')
 
+reg = re.compile(r'<h4( class="status-title")?>(.*)</h4>(.*)</script>(.*)<!-- pdf--></div>')
+imgreg = re.compile('<img class="ke_img" src="(.*?)"')
+imgnamereg = re.compile('/([^.]*?\.(jpg|png))')
 Headers = {
 		'Accept-Encoding':'gzip, deflate, sdch',
 		'Accept-Language':'en-US,en;q=0.8',
@@ -23,53 +23,104 @@ Headers = {
 		'RA-Ver':'2.8.7',
 		'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'
 		}
+
+Temp = '''
+	<html>
+	<head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>{0}</title>
+    <link href="../static/styles/reset.css" rel="stylesheet" type="text/css" />
+    <link href="../static/styles/index/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+	
+    <div class="page">
+		<table style="text-align: center;" width="90%"  align="center" border="0" cellpadding="2" cellspacing="2">
+                    <tbody>
+                        <tr>
+                            <td style=width: auto; text-align: center;"><a href="http://xueqiu.com">
+								<img src="../static/images/favicon.png" alt="xueqiu"></a></td>
+                            <td style=width: auto; text-align: center;"><h1>{0}</h1>
+								<p><a href="{1}">{2}</a>&nbsp;&nbsp;{3}</p></td>
+                        </tr>
+					</tbody>
+				</table>
+				<hr noshade="noshade" />
+				<div class="main box">	  
+				{4}</div>
+				<hr />
+				<div id="quotes" class="clearfix" style="text-align: center;">
+					<p>Copyright &copy 1996-2014 SINA Corporation All Rights Reserved. </p>
+				</div>
+			</div>
+		</body>
+	</html>'''
+
+
+def cbk(blocknum, blocksize, totalsize):
+    per = 100.0 * blocknum * blocksize / totalsize  
+    if per > 100:  
+        per = 100  
+    print '%.2f%%' % per  
 		
 def saveBlog(blogs):
 	currentpath  = os.path.realpath(__file__)
-	basedir = os.path.dirname(currentpath)
+	basedir = os.path.dirname(currentpath)	
 	for blogitem in blogs:
 		target = blogitem['target']
-		title = blogitem['title']
-		url = 'http://xueqiu.com/%s'%target
-		r = requests.get(url, headers = Headers)	
-		raw =  r.text
-		blogFolder = os.path.join(basedir, title)
+		foldername = target.split('/')[-1]
+		title = blogitem['title'] 
+		if title=='':
+			title = blogitem['description'][0:16]
+		author = blogitem['author']
+		userlink = 'http://xueqiu.com/%s'% blogitem['user_id']
+		createdtime = blogitem['created']
+		url = 'http://xueqiu.com/%s'%target		
+		blogFolder = os.path.join(basedir, foldername)		
 		if not os.path.exists(blogFolder):
 			os.mkdir(blogFolder)
-		article = re.search(reg, raw).group(3)
-		imglist = re.findall(imgreg, article)
-		# print imglist
-		for idx, imgurl in enumerate(imglist):
-			try:
-				imgpath = os.path.join(blogFolder,str(idx)+'.jpg')
-				urllib.urlretrieve(imgurl, imgpath)
-				time.sleep(2)
-				# r = requests.get(imgurl, headers = Headers)
-				# with open(imgpath, "wb") as code:
-					# code.write(r.content)
-					# time.sleep(1)
-			except Exception as e:
-				print e
-		# http://xqimg.imedao.com/14b5f1c56344053fc69d62d8.png!custom.jpg
-		# print imglist
-		#fileobj = open(local,'w')
-		#fileobj.write(li.encode('utf-8'))
-		#fileobj.close()
+			r = requests.get(url, headers = Headers)	
+			raw =  r.text
+			article = re.search(reg, raw).group(4)
+			imglist = re.findall(imgreg, article)
+			articlepath = os.path.join(blogFolder, title+'.html')
+			print title
+			for idx, imgurl in enumerate(imglist):
+				try:
+					imgname = re.search(imgnamereg, imgurl).group(1)
+					imgpath = os.path.join(blogFolder,imgname)
+					article = article.replace(imgurl, imgname)
+					urllib.urlretrieve(imgurl, imgpath, cbk)
+					time.sleep(2)
+				except Exception as e:
+					print e
+
+			html = Temp.format(title.encode('utf-8'), userlink,author.encode('utf-8'),createdtime,article.encode('utf-8'))
+			fileobj = open(articlepath,'w')
+			fileobj.write(html)
+			fileobj.close()
 	
-def blogList(url):	
-	url = url.format(1)
-	r = requests.get(url, headers = Headers)
+def blogList(id):
+	_url = "http://xueqiu.com/v4/statuses/user_timeline.json?user_id={0}&page={1}&type=2"
+	first = _url.format(id,1)	
+	r = requests.get(first, headers = Headers)
 	jdata = json.loads(r.text, encoding = 'utf-8')
-	maxpage = jdata['maxPage']
+	maxpage = jdata['maxPage']+1
 	blogs = []
-	for page in xrange(maxpage):
-		url = _url.format(page+1)  # page from 1
+	for page in xrange(1,maxpage):
+		url = _url.format(id,page)  # page from 1
 		r = requests.get(url, headers = Headers)
 		jdata = json.loads(r.text, encoding = 'utf-8')
 		articles = jdata['statuses']		
 		for article in articles:
-			blogs.append({'target':article['target'],'title':article['title']})
+			timestamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(float(article['created_at']/1000)))
+			blogs.append({'target':article['target'],'title':article['title'],'created':timestamp,'user_id':article['user_id'],'author':article['user']['screen_name'],'description':article['description']})
 	return blogs
 
-blogs = blogList(_url)
-saveBlog(blogs)
+if __name__ == '__main__':
+	idlist = ['6341375334','2821861040'] #6341375334, 2821861040
+	for id in idlist:
+		blogs = blogList(id)
+		saveBlog(blogs)
